09c2f61e1300f5b86227ff006f4b42edcd7ee88e
Amldessouki20 <amldessouki20@gmail.com>
2025-10-03 17:20:09 +0300

    update in layout and middelware

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,7 @@ const intlMiddleware = createMiddleware(routing);
 const PROTECTED_ROUTES = [
   '/dashboard', '/users', '/guests', '/cards',
   '/gates', '/reports', '/restaurants', '/scan-logs',
-  '/accommodation'
+  '/profile', '/settings','/accommodation'
 ];
 
 const PUBLIC_ROUTES = ['/', '/login', '/forgot-password', '/reset-password'];
@@ -97,9 +97,9 @@ export default async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // English routes
-    '/((?:en|ar)/)?((?:dashboard|users|guests|cards|gates|reports|restaurants|scan-logs|accommodation)/?)',
+    '/((?:en|ar)/)?((?:dashboard|users|guests|cards|gates|reports|restaurants|scan-logs|accommodation|settings)/?)',
     // Arabic routes
-    '/((?:en|ar)/)?((?:لوحة-التحكم|المستخدمين|الضيوف|البطاقات|البوابات|التقارير|المطاعم|سجلات-المسح|الاسكان|)/?)',
+    '/((?:en|ar)/)?((?:لوحة-التحكم|المستخدمين|الضيوف|البطاقات|البوابات|التقارير|المطاعم|سجلات-المسح|الاسكان|الإعدادات)/?)',
     '/',
     '/login',
     '/تسجيل-الدخول',
